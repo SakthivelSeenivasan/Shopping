222bff0e01d5984d35feff366898ca110661f192
sakthivel.seenivasa <sakthivel.seenivasan@perficient.com>
2022-05-30 19:56:25 +0530

    Added cart component and integrated data

--- src/Components/Filters.js
+++ src/Components/Filters.js
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import Rating from "./Rating";
 
 const Filters = () => {
+  const [rate, setRate] = useState(3);
   return (
     <div className='filters'>
       <span className='title'>Filter Products</span>
       <span>
         <Form.Check
           inline
-          Label='Acending'
+          label='Ascending'
           name='group1'
           type='radio'
           id={`inline-1`}
@@ -16,7 +19,7 @@ const Filters = () => {
       <span>
         <Form.Check
           inline
-          Label='Descending'
+          label='Descending'
           name='group1'
           type='radio'
           id={`inline-2`}
@@ -25,7 +28,7 @@ const Filters = () => {
       <span>
         <Form.Check
           inline
-          Label='Include Out of Stock'
+          label='Include Out of Stock'
           name='group1'
           type='checkbox'
           id={`inline-3`}
@@ -34,16 +37,20 @@ const Filters = () => {
       <span>
         <Form.Check
           inline
-          Label='FastDelivary Only'
+          label='FastDelivary Only'
           name='group1'
           type='checkbox'
           id={`inline-4`}
         />
       </span>
-      {/* <span>
+      <span>
         <label style={{ paddingRight: 10 }}>Rating</label>
-        <Rating rating={byRating} style={{ cursor: "pointer" }} />
-      </span> */}
+        <Rating
+          rating={rate}
+          onClick={(i) => setRate(i)}
+          style={{ cursor: "pointer" }}
+        />
+      </span>
       <Button variant='light'>Clear Filters</Button>
     </div>
   );
--- src/Components/SingleProduct.js
+++ src/Components/SingleProduct.js
@@ -1,5 +1,56 @@
+import { Button, Card, Col } from "react-bootstrap";
+import { CartState } from "../Context/Context";
+import Rating from "./Rating";
+
 const SingleProduct = ({ prod }) => {
-  return <div>{prod.name}</div>;
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
+  return (
+    <Col className='products' md={4}>
+      <Card>
+        <Card.Img variant='top' src={prod.image} alt={prod.name}></Card.Img>
+        <Card.Body>
+          <Card.Title>{prod.name}</Card.Title>
+          <Card.Subtitle style={{ paddingBottom: "10px" }}>
+            <span>Rs {prod.price.split(".")[0]}</span>
+            {prod.fastDelivary ? (
+              <div>Fast Delivary</div>
+            ) : (
+              <div>4 days Delivary</div>
+            )}
+            <Rating rating={prod.rating} />
+          </Card.Subtitle>
+          {cart.some((p) => p.id === prod.id) ? (
+            <Button
+              variant='danger'
+              onClick={() => {
+                dispatch({
+                  type: "REMOVE_FROM_CART",
+                  payload: prod,
+                });
+              }}
+            >
+              Remove from Cart
+            </Button>
+          ) : (
+            <Button
+              disabled={!prod.inStock}
+              onClick={() => {
+                dispatch({
+                  type: "ADD_TO_CART",
+                  payload: prod,
+                });
+              }}
+            >
+              {!prod.inStock ? "Out of Stock" : "Add to Cart"}
+            </Button>
+          )}
+        </Card.Body>
+      </Card>
+    </Col>
+  );
 };
 
 export default SingleProduct;
